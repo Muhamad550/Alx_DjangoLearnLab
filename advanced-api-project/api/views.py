35b3e5630633210a1cd4a69d711bcb5c436a1f9f
Muhamad550 <ma8287225@gmail.com>
2024-11-28 18:22:37 +0200

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -34,5 +34,3 @@ class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-
-    
